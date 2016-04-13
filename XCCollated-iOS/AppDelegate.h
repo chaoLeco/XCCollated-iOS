72171ca80961484e6fcd7a3d4bdf2b3471a683d2
Chaos <475911599@qq.com>
2016-04-13 11:01:24 +0800

    Initial Commit

---
+++ XCCollated-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCCollated-iOS
+//
+//  Created by Chaos on 16/4/13.
+//  Copyright © 2016年 Chaos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCCollated-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCCollated-iOS
+//
+//  Created by Chaos on 16/4/13.
+//  Copyright © 2016年 Chaos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
